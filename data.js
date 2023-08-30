a3c49ffd67007a78ce9d0b8a7d97c0398dce5622
Samuel Mbugua <136153354+KimaniMbugua@users.noreply.github.com>
2023-08-30 12:03:56 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Nikita Kering',
+    age: 21,
+    image:
+      'https://res.cloudinary.com/ds4opmhw5/image/upload/v1692705261/nikita_swcfr9.jpg',
+  },
+  {
+    id: 2,
+    name: 'Khaligraph Jones',
+    age: 33,
+    image:
+      'https://res.cloudinary.com/ds4opmhw5/image/upload/v1693302520/khaligraph-jones-biography-age-net-worth-wife-real-name-cars-awards-child-contact-details-2021-05-27_18-37-22_648767-ubetoo_rkl2oa.jpg',
+  },
+  {
+    id: 3,
+    name: 'Juliana Kanyamozi',
+    age: 42,
+    image:
+      'https://res.cloudinary.com/ds4opmhw5/image/upload/v1693302519/Juliana-Kanyomozi-biography_q1uyk4.jpg',
+  },
+  {
+    id: 4,
+    name: 'Octo P-I-ZZ-O',
+    age: 40,
+    image:
+      'https://res.cloudinary.com/ds4opmhw5/image/upload/v1693302520/octo_nflg9n.jpg',
+  },
+  {
+    id: 5,
+    name: 'Jeff Koinange',
+    age: 57,
+    image:
+      'https://res.cloudinary.com/ds4opmhw5/image/upload/v1693302519/BeFunky-collage-70-scaled_is04t7.jpg',
+  },
+];
